5e19abe65da46c625f47aeb5be58c5806e382de8
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-12-09 13:53:26 +0100

    creating react project, skeleton and navigation

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import NotFound from "./components/NotFound"
+
+function Main() {
+  return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomeGuest />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<Main />)
+
+if (module.hot) {
+  module.hot.accept()
+}

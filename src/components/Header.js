efe53f3c80f7a8d062f38cb7554f586651152eb0
AlexanderPanchyshyn <alexandr.panchyshyn34@gmail.com>
2022-12-12 17:29:01 +0100

    working on Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import HeaderLoggedOut from "./HeaderLoggedOut"
+import HeaderLoggedIn from "./HeaderLoggedIn"
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState()
+
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -10,19 +14,7 @@ function Header() {
             Car Diary
           </Link>
         </h4>
-        <form className="mb-0 pt-2 pt-md-0">
-          <div className="row align-items-center">
-            <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
-            </div>
-            <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-              <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
-            </div>
-            <div className="col-md-auto">
-              <button className="btn btn-success btn-sm">Log in</button>
-            </div>
-          </div>
-        </form>
+        {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
       </div>
     </header>
   )
---
+++ src/components/HeaderLoggedIn.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function HeaderLoggedIn(props) {
+  return (
+    <div className="flex-row my-3 my-md-0">
+      <button onClick={() => props.setLoggedIn(false)} className="btn btn-sm btn-secondary">
+        Sign Out
+      </button>
+    </div>
+  )
+}
+
+export default HeaderLoggedIn
